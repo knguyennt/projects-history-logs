10b326106f74cae1bdb3cc80aeeed92bdfc2c447
knguyennt <khoanguyennt2010@gmail.com>
2025-07-20 19:32:33 +0700

    feat: add vault to manage credentials

---
+++ scripts/start-vault.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+else
+  echo "❌ .env file not found!"
+  exit 1
+fi
+
+
+docker run -p $PORT:$PORT --cap-add=IPC_LOCK -d --name $CONTAINER_NAME -e "VAULT_DEV_ROOT_TOKEN_ID=$VAULT_TOKEN" -e "VAULT_DEV_LISTEN_ADDRESS=$HOST:$PORT" $IMAGE_NAME
